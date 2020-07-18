e8bf4e01d826e7bcca0b29567acbf4a55ea56887
jeonghoon <tkdgud9148@gmail.com>
2020-07-18 22:40:38 +0900

    [2020.07.18] 최종 스크립트 테스트!

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -4,13 +4,15 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-function switch_proxy() {
-    IDLE_PORT=$(find_idle_port)
+IDLE_PORT=$(find_idle_port)
 
-    echo "> 전환할 Port: $IDLE_PORT"
-    echo "> Port 전환"
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-    echo "> 엔진엑스 Reload"
-    sudo service nginx reload
-}
+if [ -z ${IDLE_PID} ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
