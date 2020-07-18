1c97ff4779ccfb5142ed5b91ca6168e8ad741a03
jeonghoon <tkdgud9148@gmail.com>
2020-07-19 00:05:21 +0900

    [2020.07.18] 변경점 확인!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,3 +37,4 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    
